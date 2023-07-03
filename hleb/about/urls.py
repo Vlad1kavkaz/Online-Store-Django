49e2ff1088e3aae419b26436f38b94ab15ab65c3
Vlad1kavkaz <162821820v@gmail.com>
2023-07-03 11:06:00 +0300

    + '/about'

--- hleb/about/urls.py
+++ hleb/about/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls')),
-    path('products-category', include('product.urls')),
-    path('about', include('about.urls')),
+    path('', views.homepage, name='home_page'),
 ]
