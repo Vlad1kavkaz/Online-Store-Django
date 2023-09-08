bf7f0997e4ab429be257f3c94c7923234c40b38b
Vlad1kavkaz <112761204+Vlad1kavkaz@users.noreply.github.com>
2023-09-08 22:20:57 +0300

    Delete hleb directory

--- hleb/main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.homepage, name='home_page'),
-    path('katalog', views.katalog, name='katalog'),
-]
--- hleb/main/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def homepage(request):
-    return render(request, 'main/home_page.html')
-
-def katalog(request):
-    return render(request, 'main/katalog.html')
